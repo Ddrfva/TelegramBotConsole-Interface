af4fa90e39516f8d9b687515f34ba5f661122725
Dorofeeva Daria <ddrfva@mail.ru>
2026-05-17 13:57:10 +0300

    Корректировка: добавила try-catch

--- Program.cs
+++ Program.cs
@@ -8,21 +8,30 @@ namespace TelegramBotConsole_Interface
     {
         static void Main(string[] args)
         {
-            Console.Write("Введите максимальное количество задач (1-100): ");
-            int maxTasks = int.Parse(Console.ReadLine());
+            try
+            {
+                Console.Write("Введите максимальное количество задач (1-100): ");
+                int maxTasks = int.Parse(Console.ReadLine());
 
-            Console.Write("Введите максимальную длину задачи (1-100): ");
-            int maxTaskLength = int.Parse(Console.ReadLine());
+                Console.Write("Введите максимальную длину задачи (1-100): ");
+                int maxTaskLength = int.Parse(Console.ReadLine());
 
-            var userService = new UserService();
-            var todoService = new ToDoService(maxTasks, maxTaskLength);
-            var botClient = new ConsoleBotClient();
-            var updateHandler = new UpdateHandler(userService, todoService);
+                var userService = new UserService();
+                var todoService = new ToDoService(maxTasks, maxTaskLength);
+                var botClient = new ConsoleBotClient();
+                var updateHandler = new UpdateHandler(userService, todoService);
 
-            botClient.StartReceiving(updateHandler);
+                botClient.StartReceiving(updateHandler);
 
-            Console.WriteLine("Бот запущен. Нажмите любую клавишу для выхода...");
-            Console.ReadKey();
+                Console.WriteLine("Бот запущен. Нажмите любую клавишу для выхода...");
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Критическая ошибка при запуске бота: {ex.Message}");
+                Console.WriteLine(ex.StackTrace);
+                Console.ReadKey();
+            }
         }
     }
 }
